030986693b6f95def98e50cfbe7ff0f8245bd788
Eduardo Inerarte <eddremonts86@gmail.com>
2026-05-15 10:38:18 +0200

    fix: add process.exit(0) to seed-db to prevent hanging DB connection

--- scripts/db/seed-db.ts
+++ scripts/db/seed-db.ts
@@ -68,7 +68,9 @@ async function main() {
   console.log('Seed complete.')
 }
 
-main().catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
